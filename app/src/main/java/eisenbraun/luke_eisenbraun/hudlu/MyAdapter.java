24fae177b58a3a2903beba314dad70100e5f17af
Luke Eisenbraun <luke.eisenbraun@hudl.com>
2016-03-14 13:01:14 -0500

    Add layout for adapter

--- app/src/main/java/eisenbraun/luke_eisenbraun/hudlu/MyAdapter.java
+++ app/src/main/java/eisenbraun/luke_eisenbraun/hudlu/MyAdapter.java
@@ -1,6 +1,8 @@
 package eisenbraun.luke_eisenbraun.hudlu;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -16,25 +18,29 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        // Create a new view
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_text_view,parent, false);
+        MyViewHolder viewHolder = new MyViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-
+        holder.mMyText.setText((mDataset[position]));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mDataset.length;
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
-        public MyViewHolder(TextView v) {
+        TextView mMyText;
+        public MyViewHolder(View v) {
             super(v);
-            mTextView = v;
+            mMyText = (TextView) v.findViewById(R.id.item_my_text);
         }
     }
 }
