c2f4611e400ee7628d0ac0ef04ddd13eb188ad60
Luke Eisenbraun <luke.eisenbraun@hudl.com>
2016-03-14 14:07:59 -0500

    Partial onClick handler

--- app/src/main/java/eisenbraun/luke_eisenbraun/hudlu/MyAdapter.java
+++ app/src/main/java/eisenbraun/luke_eisenbraun/hudlu/MyAdapter.java
@@ -1,5 +1,6 @@
 package eisenbraun.luke_eisenbraun.hudlu;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +12,11 @@ import android.widget.TextView;
  */
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private String[] mDataset;
-
     public MyAdapter(String[] myDataset){
         mDataset = myDataset;
     }
+    private OnAdapterInteractionListener mListener;
+
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -26,8 +28,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
         holder.mMyText.setText((mDataset[position]));
+
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mListener.o​nItemClicked(v, position);
+                Snackbar.make(view, myDataset[position], Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -35,12 +45,17 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         return mDataset.length;
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView mMyText;
+        mListener = (OnAdapterInteractionListener) context;
+
         public MyViewHolder(View v) {
             super(v);
             mMyText = (TextView) v.findViewById(R.id.item_my_text);
         }
+    }
+
+    public interface OnAdapterInteractionListener{
+        void o​nItemClicked(​View view, int position);
     }
 }
